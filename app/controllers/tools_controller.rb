92b990b44c11490d314716e5fce89e0a6776fd04
Ali Schlereth <aschlere@sbcglobal.net>
2017-12-20 18:29:12 -0700

    Add tools controller

---
+++ app/controllers/tools_controller.rb
@@ -0,0 +1,50 @@
+class ToolsController < ApplicationController
+
+  def index
+    @tools = Tool.all
+  end
+
+  def show
+    @tool = Tool.find(params[:id])
+  end
+
+  def new
+    @tool = Tool.new
+  end
+
+  def create
+    @tool = Tool.new(tool_params)
+    if @tool.save
+      redirect_to tool_path(@tool)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @tool = Tool.find(params[:id])
+  end
+
+  def update
+    @tool = Tool.find(params[:id])
+    @tool.update(tool_params)
+    if @tool.save
+      redirect_to tool_path(@tool)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    tool = Tool.find(params[:id])
+    tool.destroy
+    tools_path
+  end
+
+  private
+
+    def tool_params
+      params.require(:tool).permit(:type)
+    end
+
+end
